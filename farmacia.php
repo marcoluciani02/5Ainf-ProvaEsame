1d61c0ed4ddcea768dcf3d678bb839f0af5674da
marcoluciani02 <82443735+marcoluciani02@users.noreply.github.com>
2021-05-21 07:43:46 +0200

    Add files via upload

---
+++ farmacia.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Home Farmacia</title>
+        <link href="style2.css" rel="stylesheet" type="text/css" />
+        <link rel="shortcut icon" href="farmacia.ico" style="border-radius:20px 20px 20px 20px" />
+        <script src="script.js"></script>
+      </head>
+      <body>
+          <div class="hed">
+              <div class="dv">
+            <img src="Farmacia.png" alt="Immagine Farmacia" style=" width: 80px; height: 80px; ">
+            <label style="font-size: 35px; margin-left: 5px; margin-bottom: 15px;"><b>Dott. Bianchi</b></label>
+              </div>
+              <div class="dv" ><h1 style="padding-left: 37%">Farmacia</h1></div>
+              <div></div>
+          </div>
+          <div class="corpo">
+          <table border=5 cellspacing=0 background-color: cyan>
+            <tr>
+                <th> Nome Farmaco </th>
+                <th >Quantità </th>
+                <th> Descrizione </th>
+                <th> conferma </th>
+            </tr>
+            <tr>
+                <td> OKI </td>
+                <td> X103 </td>
+                <td> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultricies urna et tellus vestibulum efficitur. Pellentesque sagittis ultricies tortor, sed.</td>
+                <td> <input type="button" value="Conferma" style="Color: white; Background-color: blue"></input> </td>
+             <tr>
+             <tr>
+                <td> Tachipirina </td>
+                <td> X93 </td>
+                <td> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultricies urna et tellus vestibulum efficitur. Pellentesque sagittis ultricies tortor, sed.</td>
+                <td> <input type="button" value="Conferma" style="color: white; Background-color: blue"></input> </td>
+             <tr>
+             <tr>
+                <td> Moment ACT </td>
+                <td> X45 </td>
+                <td> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultricies urna et tellus vestibulum efficitur. Pellentesque sagittis ultricies tortor, sed.</td>
+                <td> <input type="button" value="Conferma" style="Color: white; Background-color: blue"></input> </td>
+             <tr>
+             <tr>
+                <td> Aspirina </td>
+                <td> X78 </td>
+                <td> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultricies urna et tellus vestibulum efficitur. Pellentesque sagittis ultricies tortor, sed.</td>
+                <td> <input type="button" value="Conferma" style="Color: white;Background-color: blue"></input> </td>
+             <tr>
+            </table>
+              </div>
+          </div>
+
+      </body>
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Home Ospedale</title>
+        <link href="style.css" rel="stylesheet" type="text/css" />
+        <link rel="shortcut icon" href="icona.ico" style="border-radius:20px 20px 20px 20px" />
+        <script src="script.js"></script>
+      </head>
+      <body>
+          <div class="hed">
+              <div class="dv">
+            <img src="dott.jpg" alt="Immagine dottore" style=" width: 80px; height: 80px; ">
+            <label style="font-size: 35px; margin-left: 5px; margin-bottom: 15px;"><b>Dott. Rossi</b></label>
+              </div>
+              <div class="dv" ><h1 style="padding-left: 37%">Ospedale</h1></div>
+              <div></div>
+          </div>
+          <div class="nav">
+              <div class="na" style="text-align: center; "><a href=""><label style="font-size: 35px"><b>Pazienti</b></label> </a></div>
+              <div class="na" style="text-align: center; "><a href=""><label style="font-size: 35px"><b>Visite</b></label> </a></div>
+              <div class="na" style="text-align: center; "><a href=""><label style="font-size: 35px"><b>Inserisci Pazienti</b></label> </a></div>
+              <div class="na" style="text-align: center; ">
+                    <form action="" style="margin-top: 13px;">
+                        <input type="text" placeholder="Cerca..." name="ricerca">
+                        <input type="submit" name="invio" value="Cerca" style="background-color: orange; border-radius: 5px 5px 5px 5px">
+                    </form>
+              </div>
+          </div>
+
+      </body>
+
+
+</html>
